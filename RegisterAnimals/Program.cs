4b322374146650666320fb9414f2b7f4c1bf7b8f
tmoechel-futura <t.moechel@futura-solutions.de>
2024-02-25 15:18:27 +0100

    Projektdateien hinzufügen.

---
+++ RegisterAnimals/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RegisterAnimals.Entities;
+using RegisterAnimals.Repositories;
+
+Console.WriteLine("Welcome to the Animal Registration App!");
+
+var animalRepository = new AnimalRepository();
+string animalInput = string.Empty;
+do
+{
+    Console.WriteLine("Which Animal did you see? Press L for a lion and E for an elefant. Press X to exit!");
+    animalInput = Console.ReadLine();
+
+    
+
+    if (animalInput != null && animalInput.ToLower() == "e")
+    {
+        animalRepository.AddElefant(new Elefant());
+    }
+    else if (animalInput != null && animalInput == "l")
+    {
+        animalRepository.AddLion(new Lion());
+    }
+    else if (animalInput != null && animalInput == "x")
+    {
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Please type an E for Elefant or an L for Lion");
+    }
+}
+while (animalInput.ToLower() != "x");
+
+Console.WriteLine($"Number of Lions: {animalRepository.GetLionCount()}");
+Console.WriteLine($"Number of Elefants: {animalRepository.GetElefantCount()}");
+
+
+
+
---
+++ RegisterAnimals/Repositories/AnimalRepository.cs
@@ -0,0 +1,30 @@
+﻿using RegisterAnimals.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterAnimals.Repositories
+{
+    internal class AnimalRepository
+    {
+        List<Lion> lions = new List<Lion>();
+        List<Elefant> elefants = new List<Elefant>();
+
+        public void AddLion(Lion lion)
+        {
+            lions.Add(lion);
+        }
+
+        public void AddElefant(Elefant elefant)
+        {
+            elefants.Add(elefant);
+        }
+
+        public int GetLionCount() { return lions.Count; }
+        public int GetElefantCount() { return elefants.Count; }
+
+
+    }
+}
