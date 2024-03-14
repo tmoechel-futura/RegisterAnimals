27f3a97d9494d89ade2c3c228c5ab8eefdc2450b
tmoechel-futura <t.moechel@futura-solutions.de>
2024-03-14 18:26:54 +0100

    remove some lines

--- RegisterAnimals/Program.cs
+++ RegisterAnimals/Program.cs
@@ -1,6 +1,6 @@
 ﻿///
-/// In this example we create a generic animal cage into which we can put Lions and Zebras - not a good idea though ;-)
-/// We first add one Zebra and then one Lion and show the first animal in the cage which is a Zebra 
+/// In this example we want to create 2 generic animal cages into which we can put either Lions or Zebras - so they will not eat each other ;-)
+/// 
 ///
 
 // See https://aka.ms/new-console-template for more information
@@ -9,15 +9,7 @@ using RegisterAnimals.Repositories;
 
 Console.WriteLine("Welcome to the Animal Registration App!");
 
-/// *** Importanct concept to understand *** /
-// understand object assignment
-Animal animal = new Zebra();  // OK  a Zebra is animal
-
-// Zebra zebra = new Animal();  // NOT OK not every animal is a Zebra, so an Animal is not generally a Zebra
-/// ****************************************** //
-/// 
-
-// I want to put animal into a Cage<T> 
+// I create a cage for Lions
 ICage<Animal> animalCage = new Cage<Animal>();  // OK Interface Types are non variant
 
 // This does not work, if I create a Zebra cage I can not put any animals inside  
