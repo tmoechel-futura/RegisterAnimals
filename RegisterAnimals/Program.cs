c17775e2e9b0eb1521a25c5ca697640b6b4d905a
tmoechel-futura <t.moechel@futura-solutions.de>
2024-03-15 09:10:11 +0100

    fix comment

--- RegisterAnimals/Program.cs
+++ RegisterAnimals/Program.cs
@@ -10,7 +10,7 @@ using RegisterAnimals.Repositories;
 Console.WriteLine("Welcome to the Animal Registration App!");
 
 // I create a cage for Lions
-// OK if Interface Types are covariant because I use a more specific type (Lion) to be used as the generic Type 
+// OK if Interface Types are covariant because I use a less specific type (Animal) as the generic Interface Type ICage<Animal> cage
 // But a covariant interface can only access the value which means you can read data, but you can not add data
 // This makes sense because you do not want to add a Zebra into a Lions Cage (in general you do not want to be able to add another specific type)
 // The interface definition now contains the out keyword: public interface ICage<out T> where T : Animal
